e6265c1ae43375a79bee0f5bf9798aa192d1fcc6
Maya OH <mayaoh2000@gmail.com>
2024-04-01 03:10:12 -0400

    Training cnn modle and saving model done

--- cnnModel.py
+++ cnnModel.py
@@ -1,29 +1,37 @@
 #Contains the actual cnn model and training process
 
+from matplotlib import pyplot as plt
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import ConfusionMatrixDisplay
+from skorch import NeuralNetClassifier
 from torch.utils.data import DataLoader, Subset, random_split
 import torch
 import torchvision
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torchvision.datasets
+import torch.optim as optim
 
 #data directory for local computer for dataset
 dataPath = "C:/Users/mayao/Desktop/Comp 472 - Artificiall intelligence/Project/Comp472_AI-Project/Dataset/train"
 
 #path to save the model that is being train
-modelPath = "C:/Users/mayao/Desktop/Comp 472 - Artificiall intelligence/Project/Comp472_AI-Project/Dataset/Models"
+modelPath = "C:/Users/mayao/Desktop/Comp 472 - Artificiall intelligence/Project/Comp472_AI-Project/Dataset/Models/"
 
 #Seting up the pretraining-process
 #Hyperparameters
 num_epochs = 10     #minimum of 10 iteration
 num_classes = 4     #total number of classes
 learningRate = 0.001          #learnin rate for the model
-classes = ("angry", "neutral", "engaged", "surpise")    #classes for classification
+batchSize = 32
 
 #transform
 transform = transforms.Compose(
-    [transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    [transforms.Grayscale(num_output_channels=1),   #Images are in grayscaled
+    transforms.Resize((48,48)),#image size 48 x 48
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))])
 
 #getting dataset for model to train on
 dataset = torchvision.datasets.ImageFolder(dataPath, transform=transform)
@@ -31,9 +39,14 @@ dataset = torchvision.datasets.ImageFolder(dataPath, transform=transform)
 #randomely spliting the datset into training and testing (80% for training and 20 % for testing)
 m = len(dataset)
 trainData, testData = random_split(dataset, [int (m-m*0.2), int(m*0.2)])
+trainLoader = DataLoader(trainData, batch_size=32, shuffle=True, num_workers=2)
+testLoader = DataLoader(testData, batch_size=32, shuffle=True, num_workers=2)
 
 #checking if user has cude to be able to use GPU instead of CPU for training
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+y_train = np.array ([y for x, y,in iter (trainData)])
+classes = ("angry", "neutral", "engaged", "surpise")    #classes for classification
 
 #Doc string used here for documentation purposes
 """++++++ CNN Class Starts here ++++++ 
@@ -88,47 +101,71 @@ Showing the accuracy of the model on the test dataset.
 
 #creating an instance of the cnn model created from above
 model = CNN()
-criterion = nn.CrossEntropyLoss
-optimizer = torch.optim.Adam(model.parameter(), lr = learningRate)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = learningRate)
 
 #training modle
-total_step = len(trainData)
-loss_list = []
-acc_list = []
-for epoch in range(num_epochs):
- for i, (images, labels) in enumerate(trainData):
-        # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        loss_list.append(loss.item())
+# total_step = len(trainLoader)
+# loss_list = []
+# acc_list = []
 
-        # Backprop and optimisation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+# for epoch in range(num_epochs):
+#  for i, (images, labels) in enumerate(trainLoader):
 
-        # Train accuracy
-        total = labels.size(0)
-        _, predicted = torch.max(outputs.data, 1)
-        correct = (predicted == labels).sum().item()
-        acc_list.append(correct / total)
+#         # Forward pass
+#         outputs = model(images)
+#         loss = criterion(outputs, labels)
+#         loss_list.append(loss.item())
+
+#         # Backprop and optimisation
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+
+#         # Train accuracy
+#         total = labels.size(0)
+#         _, predicted = torch.max(outputs.data, 1)
+#         correct = (predicted == labels).sum().item()
+#         acc_list.append(correct / total)
             
-        print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
-            .format(epoch + 1, num_epochs, i + 1, total_step, loss.item(),
-            (correct / total) * 100))
+#         print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
+#             .format(epoch + 1, num_epochs, i + 1, total_step, loss.item(),
+#             (correct / total) * 100))
+torch.manual_seed(0)
+net = NeuralNetClassifier(
+    CNN(),
+    max_epochs=num_epochs,
+    lr=learningRate,
+    batch_size=batchSize,
+    optimizer=optim.Adam,
+    criterion=nn.CrossEntropyLoss,
+    device=device,
+    iterator_train__shuffle=True
+)
+y_train = np.array([np.int64(y) for x, y in iter(trainData)])
+net.fit(trainData, y=y_train)
+print("\nFinished Training!!")
         
 #testing model that just been trained
-model.eval()
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in testData:
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-print('Test Accuracy of the model on the test images: {} %'
-.format((correct / total) * 100))
+y_predict = net.predict(testData)
+y_test = np.array([y for x, y in iter(testData)])
+print('Accuracy for {} Dataset: {}%'.format('Test', round(accuracy_score(y_test, y_predict) * 100, 2)))
+ConfusionMatrixDisplay.from_predictions(y_test, y_predict, display_labels=classes)
+plt.title('Confusion Matrix for {} Dataset'.format('Test'))
+plt.show()
+
+
+# model.eval()
+# with torch.no_grad():
+#     correct = 0
+#     total = 0
+#     for images, labels in testLoader:
+#         outputs = model(images)
+#         _, predicted = torch.max(outputs.data, 1)
+#         total += labels.size(0)
+#         correct += (predicted == labels).sum().item()
+# print('Test Accuracy of the model on the test images: {} %'
+# .format((correct / total) * 100))
 
 #saving the model
 torch.save(model.state_dict(), modelPath)
